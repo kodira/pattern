a6feb95a4a79078a52bee88ea6c26f42d1574307
Cornelius Hald <cornelius.hald@tourschall.com>
2012-12-26 17:36:07 +0100

    Updated German translation

--- translations/Pattern_de.ts
+++ translations/Pattern_de.ts
@@ -6,16 +6,16 @@
     <message>
         <source>Pattern lets you set your wallpaper to beautiful patterns and colors. Written by Cornelius Hald for Kodira.
 This program uses the COLOURlovers.com API. All patterns are licensed under CC-BY-NC-SA.</source>
-        <translation type="unfinished">Mit &apos;Pattern&apos; kannst du tolle Muster und Farben als Hintergrundbild setzten. Geschrieben von Cornelius Hald für Kodira.
+        <translation>Mit &apos;Pattern&apos; kannst du tolle Muster und Farben als Hintergrundbild setzten. Geschrieben von Cornelius Hald für Kodira.
 Diese app nutzt die COLOURlovers.com API. Alle Muster stehen unter CC-BY-NC-SA Lizenz.</translation>
     </message>
     <message>
         <source>Website</source>
-        <translation type="unfinished"></translation>
+        <translation>Website</translation>
     </message>
     <message>
         <source>More Apps</source>
-        <translation type="unfinished"></translation>
+        <translation>Mehr Apps</translation>
     </message>
 </context>
 <context>
@@ -48,37 +48,37 @@ Diese app nutzt die COLOURlovers.com API. Alle Muster stehen unter CC-BY-NC-SA L
     <name>DetailsPage</name>
     <message>
         <source>As Wallpaper</source>
-        <translation type="unfinished">Als Hintergrund</translation>
+        <translation>Als Hintergrund</translation>
     </message>
     <message>
         <source>Share</source>
-        <translation type="unfinished">Teilen</translation>
+        <translation>Teilen</translation>
     </message>
     <message>
         <source>by</source>
-        <translation type="unfinished">von</translation>
+        <translation>von</translation>
     </message>
 </context>
 <context>
     <name>PatternItem</name>
     <message>
         <source>by</source>
-        <translation type="unfinished">von</translation>
+        <translation>von</translation>
     </message>
     <message>
         <source>Loading more patterns...</source>
-        <translation type="unfinished"></translation>
+        <translation>Lade weitere Muster...</translation>
     </message>
 </context>
 <context>
     <name>PatternsPage</name>
     <message>
         <source>Top Rated</source>
-        <translation type="unfinished">Beliebt</translation>
+        <translation>Beliebt</translation>
     </message>
     <message>
         <source>Newest</source>
-        <translation type="unfinished">Neu</translation>
+        <translation>Neu</translation>
     </message>
     <message>
         <source>Load previous page</source>
